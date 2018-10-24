38d7c06fc85c298e48a578ccf4a25b7e7b8bbdc6
Rasmus Kaj <kaj@kth.se>
2018-10-24 15:13:50 +0200

    Use sensehat_screen directly.

--- src/main.rs
+++ src/main.rs
@@ -2,36 +2,18 @@ extern crate env_logger;
 #[macro_use]
 extern crate log;
 extern crate reqwest;
-#[cfg(feature = "sensehat")]
-extern crate sensehat;
 extern crate sensehat_screen;
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 
-#[cfg(feature = "sensehat")]
-use sensehat::{Colour, SenseHat, SenseHatResult};
+use sensehat_screen::{PixelColor, PixelFrame};
+use sensehat_screen::framebuffer::FramebufferError;
 
 fn main() {
     env_logger::init();
-    if let Err(e) = hello_hat() {
-        eprintln!("Error on hat: {:?}", e);
-    }
     let state = MonitorState::load();
     show_state(state).unwrap();
-}
-
-#[cfg(feature = "sensehat")]
-fn hello_hat() -> SenseHatResult<()> {
-    let mut hat = SenseHat::new()?;
-    println!("Pressure: {:?}", hat.get_pressure());
-    hat.text(" Hello from Rust!  ", Colour::RED, Colour::BLACK)?;
-    Ok(())
-}
-
-#[cfg(not(feature = "sensehat"))]
-fn hello_hat() -> Result<(), ()> {
-    Ok(())
 }
 
 #[derive(Debug)]
@@ -67,19 +49,24 @@ impl MonitorState {
     }
 }
 
-#[cfg(not(feature = "sensehat"))]
-fn show_state(state: MonitorState) -> Result<(), ()> {
-    eprintln!("Status is {:?}", state);
-    Ok(())
-}
-
-#[cfg(feature = "sensehat")]
-fn show_state(state: MonitorState) -> SenseHatResult<()> {
+fn show_state(state: MonitorState) -> Result<(), FramebufferError> {
     eprintln!("Status is {:?}", state);
     let mut screen =
-        sensehat_screen::Screen::open("/dev/fb1").map_err(|_| SenseHatError::ScreenError)?;
+        sensehat_screen::Screen::open("/dev/fb1")?;
+    let n = PixelColor::BLACK;
+    let b = PixelColor::BLUE;
+    let r = PixelColor::RED;
     let img = match state {
-        MonitorState::Happy => PixelFrame::BLUE,
+        MonitorState::Happy => PixelFrame::from([
+            n, n, n, n, n, n, n, n,
+            n, b, n, n, n, n, b, n,
+            n, n, n, n, n, n, n, n,
+            n, n, n, n, n, n, n, n,
+            r, n, n, n, n, n, n, r,
+            n, r, n, n, n, n, r, n,
+            n, n, r, r, r, r, n, n,
+            n, n, n, n, n, n, n, n,
+        ]),
         MonitorState::Neutral => PixelFrame::BLACK,
         MonitorState::Sad => PixelFrame::RED,
         MonitorState::CheckFailed => PixelFrame::YELLOW,
